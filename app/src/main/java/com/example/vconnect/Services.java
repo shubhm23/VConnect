a6d04d25c2645d8ec213db626dd40ee3a1428deb
shubhm23 <1ms19cs118@gmail.com>
2022-04-10 19:26:34 +0530

    Add Screen Mirroring

--- app/src/main/java/com/example/vconnect/Services.java
+++ app/src/main/java/com/example/vconnect/Services.java
@@ -1,6 +1,8 @@
 package com.example.vconnect;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
@@ -9,14 +11,15 @@ import androidx.annotation.Nullable;
 
 public class Services extends Activity {
     Button b1, b2, b3, b4;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
-        b1 = findViewById(R.id.button);
-        b2 = findViewById(R.id.button2);
-        b3 = findViewById(R.id.button3);
-        b4 = findViewById(R.id.button4);
+        b1 = findViewById(R.id.btnWifi);
+        b2 = findViewById(R.id.btnRemoteSetting);
+        b3 = findViewById(R.id.btnScreenMirror);
+        b4 = findViewById(R.id.btnStorage);
 
         b1.setOnClickListener(view -> {
             Toast.makeText(Services.this, "Wifi and Bluetooth Settings", Toast.LENGTH_SHORT).show();
@@ -27,6 +30,7 @@ public class Services extends Activity {
         });
 
         b3.setOnClickListener(view -> {
+            enablingWiFiDisplay();
             Toast.makeText(Services.this, "Screen Mirroring", Toast.LENGTH_SHORT).show();
         });
 
@@ -34,61 +38,21 @@ public class Services extends Activity {
             Toast.makeText(Services.this, "Storage", Toast.LENGTH_SHORT).show();
         });
     }
-}
 
-//public class RegisterActivity extends Activity {
-//
-//    TextInputEditText etRegEmail;
-//    TextInputEditText etRegPassword;
-//    TextView tvLoginHere;
-//    Button btnRegister;
-//
-//    FirebaseAuth mAuth;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_register);
-//
-//        etRegEmail = findViewById(R.id.etRegEmail);
-//        etRegPassword = findViewById(R.id.etRegPass);
-//        tvLoginHere = findViewById(R.id.tvLoginHere);
-//        btnRegister = findViewById(R.id.btnRegister);
-//
-//        mAuth = FirebaseAuth.getInstance();
-//
-//        btnRegister.setOnClickListener(view ->{
-//            createUser();
-//        });
-//
-//        tvLoginHere.setOnClickListener(view ->{
-//            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-//        });
-//    }
-//
-//    private void createUser(){
-//        String email = etRegEmail.getText().toString();
-//        String password = etRegPassword.getText().toString();
-//
-//        if (TextUtils.isEmpty(email)){
-//            etRegEmail.setError("Email cannot be empty");
-//            etRegEmail.requestFocus();
-//        }else if (TextUtils.isEmpty(password)){
-//            etRegPassword.setError("Password cannot be empty");
-//            etRegPassword.requestFocus();
-//        }else{
-//            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                @Override
-//                public void onComplete(@NonNull Task<AuthResult> task) {
-//                    if (task.isSuccessful()){
-//                        Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
-//                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-//                    }else{
-//                        Toast.makeText(RegisterActivity.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            });
-//        }
-//    }
-//
-//}
+    public void enablingWiFiDisplay() {
+        try {
+            startActivity(new Intent("android.settings.WIFI_DISPLAY_SETTINGS"));
+            return;
+        } catch (ActivityNotFoundException activitynotfoundexception) {
+            activitynotfoundexception.printStackTrace();
+        }
+
+        try {
+            startActivity(new Intent("android.settings.CAST_SETTINGS"));
+            return;
+        } catch (Exception exception1) {
+            Toast.makeText(getApplicationContext(), "Device not supported", Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
